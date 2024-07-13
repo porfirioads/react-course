8593da89c2300929158e763fe4f487a96073a3a3
Porfirio Ángel Díaz Sánchez <porfirioads@gmail.com>
2024-07-13 13:47:35 -0600

    feat: add examples of variables and template strings

---
+++ 02-intro-javascript/src/bases/01-const-let.js
@@ -0,0 +1,6 @@
+const nombre = "Porfirio";
+const apellido = "Díaz";
+let valorDado = 5;
+valorDado = 4;
+
+console.log(nombre, apellido, valorDado);
---
+++ 02-intro-javascript/src/bases/02-template-string.js
@@ -0,0 +1,12 @@
+const nombre = "Porfirio";
+const apellido = "Díaz";
+
+const nombreCompleto = `${nombre} ${apellido}`;
+
+console.log(nombreCompleto);
+
+function getSaludo() {
+  return "Hola mundo";
+}
+
+console.log(`Este es un texto: ${getSaludo()}`)
