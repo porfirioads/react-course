16d257478574cc42dc2ec64e3bbd6d2db0b7906c
Porfirio Ángel Díaz Sánchez <porfirioads@gmail.com>
2024-07-29 09:50:49 -0600

    feat: add functions

---
+++ 02-intro-javascript/src/bases/05-functions.js
@@ -0,0 +1,45 @@
+const greet = function (name) {
+  return `Hola, ${name}`;
+};
+
+const greet2 = (name) => {
+  return `Hola, ${name}`;
+};
+
+const greet3 = (name) => `Hola, ${name}`;
+
+console.log(greet("Goku"));
+console.log(greet2("vegeta"));
+console.log(greet3("Gohan"));
+
+const getUser = () => {
+  return {
+    uid: "ABC123",
+    username: "El_User12",
+  };
+};
+
+const getUser2 = () => ({
+  uid: "123ABC",
+  username: "12El_User",
+});
+
+console.log(getUser());
+console.log(getUser2());
+
+function getActiveUser(name) {
+  return {
+    uid: "ABC567",
+    username: name,
+  };
+}
+
+const activeUser = getActiveUser("Porfirio");
+console.log(activeUser);
+
+const getActiveUser2 = (name) => ({
+  uid: "ABC567",
+  username: name,
+});
+
+console.log(getActiveUser2('Angel'))
--- 02-intro-javascript/src/index.js
+++
@@ -1,4 +0,0 @@
-
-function greet(name) {
-    return `Hola, ${name}`
-}
