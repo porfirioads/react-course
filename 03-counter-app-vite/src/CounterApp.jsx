4a65e185abf29240ec31cc027b48448f9d16059d
Porfirio Ángel Díaz Sánchez <porfirioads@gmail.com>
2024-08-21 13:45:29 -0600

    feat: add useState hook

--- 03-counter-app-vite/src/CounterApp.jsx
+++ 03-counter-app-vite/src/CounterApp.jsx
@@ -1,10 +1,20 @@
+import { useState } from 'react';
 import PropTypes from 'prop-types';
 
 export const CounterApp = ({ value }) => {
+  const [counter, setCounter] = useState(value);
+
+  const handleAdd = (event) => {
+    console.log('+1', event);
+    setCounter(counter + 1);
+  };
+
   return (
     <>
       <h1>CounterApp</h1>
-      <h2>{value}</h2>
+      <h2>{counter}</h2>
+      {/* Args from onClick will pass to handleAdd */}
+      <button onClick={handleAdd}>+1</button>
     </>
   );
 };
