74fe8ba6d4c1600f41509394b511cf02cc693ff9
Porfirio Ángel Díaz Sánchez <porfirioads@gmail.com>
2024-09-09 17:21:56 -0600

    feat: add getAllByText test

--- 03-counter-app-vite/src/FirstApp.jsx
+++ 03-counter-app-vite/src/FirstApp.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 export const FirstApp = ({ title, subTitle, name }) => {
   return (
     <>
-      <h1>{title}</h1>
+      <h1 data-testid='test-title'>{title}</h1>
       <p>{subTitle}</p>
       <p>{name}</p>
     </>
--- 03-counter-app-vite/tests/FirstApp.test.jsx
+++ 03-counter-app-vite/tests/FirstApp.test.jsx
@@ -2,17 +2,37 @@ import { render } from '@testing-library/react';
 import { FirstApp } from '../src/FirstApp';
 
 describe('Pruebas en <FirstApp />', () => {
-  test('Debe hacer match con el snapshot', () => {
-    const title = 'Hola, soy Goku';
-    const { container } = render(<FirstApp title={title} />);
-    expect(container).toMatchSnapshot();
-  });
+  // test('Debe hacer match con el snapshot', () => {
+  //   const title = 'Hola, soy Goku';
+  //   const { container } = render(<FirstApp title={title} />);
+  //   expect(container).toMatchSnapshot();
+  // });
 
   test('Debe mostrar el título en un h1', () => {
     const title = 'Hola, soy Goku';
-    const { container, getByText } = render(<FirstApp title={title} />);
+
+    const { container, getByText, getByTestId } = render(
+      <FirstApp title={title} />,
+    );
+
     expect(getByText(title)).toBeTruthy();
+
     const h1 = container.querySelector('h1');
     expect(h1.innerHTML).toContain(title);
+
+    expect(getByTestId('test-title').innerHTML).toBe(title); // Exact match
+    expect(getByTestId('test-title').innerHTML).toContain(title);
+  });
+
+  test('Debe mostrar el subtítulo enviado por props', () => {
+    const title = 'Hola, soy Goku';
+    const subTitle = 'Hola, soy un subtítulo';
+
+    const { getByText, getAllByText } = render(
+      <FirstApp title={title} subTitle={subTitle} />,
+    );
+
+    expect(getByText(subTitle)).toBeTruthy();
+    expect(getAllByText(subTitle).length).toBe(1);
   });
 });
