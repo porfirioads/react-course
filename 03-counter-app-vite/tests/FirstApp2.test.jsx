046fe1ac536e6f0d65ed0d6575fca4a5fa2376c5
Porfirio Ángel Díaz Sánchez <porfirioads@gmail.com>
2024-09-09 17:35:43 -0600

    feat: use screen object in tests

---
+++ 03-counter-app-vite/tests/FirstApp2.test.jsx
@@ -0,0 +1,28 @@
+import { render, screen } from '@testing-library/react';
+import { FirstApp } from '../src/FirstApp';
+
+describe('Pruebas en <FirstApp />', () => {
+  const title = 'Hola, soy Goku';
+  const subTitle = 'Hola, soy un subtítulo';
+
+  test('Debe hacer match con el snapshot', () => {
+    const { container } = render(<FirstApp title={title} />);
+    expect(container).toMatchSnapshot();
+  });
+
+  test('Debe mostrar el mensaje "Hola, Soy Goku"', () => {
+    render(<FirstApp title={title} />);
+    expect(screen.getByText(title)).toBeTruthy();
+  });
+
+  test('Debe mostrar el título en un h1', () => {
+    render(<FirstApp title={title} />);
+    expect(screen.getByRole('heading', {level: 1}).innerHTML).toContain(title)
+  });
+
+  test('Debe mostrar el subtítulo enviado por props', () => {
+    render(<FirstApp title={title} subTitle={subTitle} />);
+    expect(screen.getByText(subTitle)).toBeTruthy();
+    expect(screen.getAllByText(subTitle).length).toBe(1);
+  });
+});
