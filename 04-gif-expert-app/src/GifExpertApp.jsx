64a57d22d01bbfbdd9e41535447c338c8f79928a
Porfirio Ángel Díaz Sánchez <porfirioads@gmail.com>
2024-09-11 21:34:21 -0600

    feat: create gif expert app

---
+++ 04-gif-expert-app/src/GifExpertApp.jsx
@@ -0,0 +1,28 @@
+export const GifExpertApp = () => {
+  return (
+    <>
+      {/* Title */}
+      <h1>GifExpertApp</h1>
+
+      {/* Input */}
+
+      {/* Gif list */}
+    </>
+  );
+};
+
+// const apiKey = 'J1ZoQbpMEFkteFvsKuxy475UYEv2ngyw';
+
+// const randomGifRequest = fetch(
+//   `https://api.giphy.com/v1/gifs/random?api_key=${apiKey}`,
+// );
+
+// randomGifRequest
+//   .then((response) => response.json())
+//   .then(({data}) => {
+//     const { url } = data.images.original;
+//     const img = document.createElement('img');
+//     img.src = url;
+//     document.body.append(img)
+//   })
+//   .catch((error) => console.error);
