65da84a81242903c4533b552d9dd00f35a25b346
Porfirio Ángel Díaz Sánchez <porfirioads@gmail.com>
2024-09-19 22:54:46 -0600

    feat: set object state using spread

--- 05-hook-app/src/main.jsx
+++ 05-hook-app/src/main.jsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { HookApp } from './HookApp.jsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+// import { HookApp } from './HookApp.jsx'
+import "./index.css";
+import { CounterApp } from "./01-useState/CounterApp";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <HookApp />
-  </StrictMode>,
-)
+    <CounterApp />
+    {/* <HookApp /> */}
+  </StrictMode>
+);
