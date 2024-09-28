ba4749a2336e6fc1248b6131b6f842bdbd423cc7
Porfirio Ángel Díaz Sánchez <porfirioads@gmail.com>
2024-09-28 15:19:05 -0600

    feat: add useFetch custom hook

---
+++ 05-hook-app/src/hooks/useFetch.js
@@ -0,0 +1,50 @@
+import { useEffect, useState } from 'react';
+
+export const useFetch = (url) => {
+  const [state, setState] = useState({
+    data: null,
+    isLoading: true,
+    hasError: false,
+    error: null,
+  });
+
+  useEffect(() => {
+    getFetch();
+  }, []);
+
+  const setLoadingState = () => {
+    setState({
+      data: null,
+      isLoading: true,
+      hasError: false,
+      error: null,
+    });
+  };
+
+  const getFetch = async () => {
+    setLoadingState();
+
+    const response = await fetch(url);
+
+    await new Promise((resolve) => setTimeout(resolve, 1500));
+
+    if (!response.ok) {
+      setState({
+        data: null,
+        isLoading: false,
+        hasError: true,
+        error: response.status,
+      });
+      return;
+    }
+
+    const data = await response.json();
+    setState({ data, isLoading: false, hasError: false, error: null });
+  };
+
+  return {
+    data: state.data,
+    isLoading: state.isLoading,
+    hasError: state.hasError,
+  };
+};
--- 05-hook-app/src/main.jsx
+++ 05-hook-app/src/main.jsx
@@ -5,7 +5,8 @@ import './index.css';
 // import { CounterApp } from "./01-useState/CounterApp";
 // import { CounterWithCustomHook } from "./01-useState/CounterWithCustomHook";
 // import { SimpleForm } from './02-useEffect/SimpleForm';
-import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook';
+// import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook';
+import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks';
 
 createRoot(document.getElementById('root')).render(
   // {/* <StrictMode> */}
@@ -13,6 +14,7 @@ createRoot(document.getElementById('root')).render(
   // {/* <CounterWithCustomHook /> */}
   // {/* <HookApp /> */}
   // <SimpleForm />
-  <FormWithCustomHook />,
+  // <FormWithCustomHook />,
+  <MultipleCustomHooks />,
   // {/* </StrictMode> */}
 );
