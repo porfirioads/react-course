2368deaf63a26e6ed4a1f1e37825409519432eac
Porfirio Ángel Díaz Sánchez <porfirioads@gmail.com>
2024-09-28 12:54:53 -0600

    feat: show coords

--- 05-hook-app/src/02-useEffect/Message.jsx
+++ 05-hook-app/src/02-useEffect/Message.jsx
@@ -1,9 +1,11 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 export const Message = () => {
+  const [coords, setCoords] = useState({ x: 0, y: 0 });
+  
   useEffect(() => {
     const onMouseMove = ({ x, y }) => {
-      console.log({ x, y });
+      setCoords({ x, y });
     };
 
     window.addEventListener('mousemove', onMouseMove);
@@ -16,6 +18,7 @@ export const Message = () => {
   return (
     <>
       <h3>Usuario ya existe</h3>
+      <p>{JSON.stringify(coords)}</p>
     </>
   );
 };
