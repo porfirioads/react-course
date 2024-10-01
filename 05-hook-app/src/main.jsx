0f9ada2a4a3b24847fff857c8f5a4bd90a29b8cd
Porfirio Ángel Díaz Sánchez <porfirioads@gmail.com>
2024-10-01 11:07:22 -0600

    feat: add example of useMemo hook

--- 05-hook-app/src/main.jsx
+++ 05-hook-app/src/main.jsx
@@ -1,8 +1,6 @@
 // import { StrictMode } from "react";
 import { createRoot } from 'react-dom/client';
 // import { HookApp } from './HookApp.jsx'
-import './index.css';
-import { Memorize } from './06-memos/Memorize';
 // import { CounterApp } from "./01-useState/CounterApp";
 // import { CounterWithCustomHook } from "./01-useState/CounterWithCustomHook";
 // import { SimpleForm } from './02-useEffect/SimpleForm';
@@ -10,6 +8,10 @@ import { Memorize } from './06-memos/Memorize';
 // import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks';
 // import { FocusScreen } from './04-useRef/FocusScreen';
 // import { Layout } from './05-useLayoutEffect/Layout';
+// import { Memorize } from './06-memos/Memorize';
+ import { MemoHook } from './07-memoHook/MemoHook';
+
+import './index.css';
 
 createRoot(document.getElementById('root')).render(
   // {/* <StrictMode> */}
@@ -21,6 +23,6 @@ createRoot(document.getElementById('root')).render(
   // <MultipleCustomHooks />,
   // <FocusScreen />
   // <Layout />
-  <Memorize />,
+  <MemoHook />,
   // {/* </StrictMode> */}
 );
