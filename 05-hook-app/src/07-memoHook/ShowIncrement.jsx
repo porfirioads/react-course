4d208d4549773b7ffce3b4c70563fa2c4de7472b
Porfirio Ángel Díaz Sánchez <porfirioads@gmail.com>
2024-10-01 11:23:33 -0600

    feat: add example of useCallback hook

---
+++ 05-hook-app/src/07-memoHook/ShowIncrement.jsx
@@ -0,0 +1,23 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+
+export const ShowIncrement = React.memo(({ increment }) => {
+  console.log('<ShowIncrement /> se dibujó');
+
+  return (
+    <button
+      className="btn btn-primary"
+      onClick={() => {
+        increment();
+      }}
+    >
+      Incrementar
+    </button>
+  );
+});
+
+ShowIncrement.propTypes = {
+  increment: PropTypes.func.isRequired,
+};
+
+ShowIncrement.displayName = 'ShowIncrement';
--- 05-hook-app/src/main.jsx
+++ 05-hook-app/src/main.jsx
@@ -1,5 +1,6 @@
 // import { StrictMode } from "react";
 import { createRoot } from 'react-dom/client';
+import './index.css';
 // import { HookApp } from './HookApp.jsx'
 // import { CounterApp } from "./01-useState/CounterApp";
 // import { CounterWithCustomHook } from "./01-useState/CounterWithCustomHook";
@@ -9,9 +10,8 @@ import { createRoot } from 'react-dom/client';
 // import { FocusScreen } from './04-useRef/FocusScreen';
 // import { Layout } from './05-useLayoutEffect/Layout';
 // import { Memorize } from './06-memos/Memorize';
- import { MemoHook } from './07-memoHook/MemoHook';
-
-import './index.css';
+// import { MemoHook } from './07-memoHook/MemoHook';
+import { CallbackHook } from './07-memoHook/CallbackHook';
 
 createRoot(document.getElementById('root')).render(
   // {/* <StrictMode> */}
@@ -23,6 +23,7 @@ createRoot(document.getElementById('root')).render(
   // <MultipleCustomHooks />,
   // <FocusScreen />
   // <Layout />
-  <MemoHook />,
+  // <MemoHook />,
+  <CallbackHook />,
   // {/* </StrictMode> */}
 );
