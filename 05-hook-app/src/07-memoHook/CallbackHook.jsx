c5eb57d02fd8399f57fc6009d0b973faa2bba1f3
Porfirio Ángel Díaz Sánchez <porfirioads@gmail.com>
2024-10-01 11:28:03 -0600

    feat: add arguments to useCallback hook

--- 05-hook-app/src/07-memoHook/CallbackHook.jsx
+++ 05-hook-app/src/07-memoHook/CallbackHook.jsx
@@ -1,16 +1,20 @@
-import { useCallback, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import { ShowIncrement } from './ShowIncrement';
 
 export const CallbackHook = () => {
   const [counter, setCounter] = useState(10);
 
-  const increment = useCallback(() => {
-    setCounter((value) => value + 1);
+  const increment = useCallback((value) => {
+    setCounter((c) => c + value);
   }, []);
 
   //   const increment = () => {
   //     setCounter(counter + 1);
   //   };
+
+  useEffect(() => {
+    // increment()
+  }, [increment]);
 
   return (
     <>
