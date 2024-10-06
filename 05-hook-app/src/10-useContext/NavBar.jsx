91410200f0acf7ee10b2f306d3ccf3e44feb4899
Porfirio Ángel Díaz Sánchez <porfirioads@gmail.com>
2024-10-06 16:57:55 -0600

    feat: add basic nav links for navbar

--- 05-hook-app/src/10-useContext/NavBar.jsx
+++ 05-hook-app/src/10-useContext/NavBar.jsx
@@ -1,11 +1,56 @@
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 
 export const NavBar = () => {
   return (
     <>
-      <Link to="/about">About</Link>
-      <Link to="/login">Login</Link>
-      <Link to="/">Home</Link>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark rounded-3">
+        <div className="container-fluid">
+          <Link className="navbar-brand" to="/">
+            useContext
+          </Link>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNavAltMarkup"
+            aria-controls="navbarNavAltMarkup"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div className="navbar-nav">
+              <NavLink
+                className={({ isActive }) =>
+                  `nav-link ${isActive ? 'active' : ''}`
+                }
+                to="/"
+              >
+                Home
+              </NavLink>
+
+              <NavLink
+                className={({ isActive }) =>
+                  `nav-link ${isActive ? 'active' : ''}`
+                }
+                to="/about"
+              >
+                About
+              </NavLink>
+
+              <NavLink
+                className={({ isActive }) =>
+                  `nav-link ${isActive ? 'active' : ''}`
+                }
+                to="/login"
+              >
+                Login
+              </NavLink>
+            </div>
+          </div>
+        </div>
+      </nav>
     </>
   );
 };
