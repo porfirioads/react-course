f57e52a67b14650599c5c9565559b54225db5162
Porfirio Ángel Díaz Sánchez <porfirioads@gmail.com>
2024-10-06 16:45:28 -0600

    feat: add base navbar

--- 05-hook-app/src/10-useContext/MainApp.jsx
+++ 05-hook-app/src/10-useContext/MainApp.jsx
@@ -2,11 +2,13 @@ import { Navigate, Route, Routes } from 'react-router-dom';
 import { HomePage } from './HomePage';
 import { AboutPage } from './AboutPage';
 import { LoginPage } from './LoginPage';
+import { NavBar } from './NavBar';
 
 export const MainApp = () => {
   return (
     <>
       <h1>MainApp</h1>
+      <NavBar/>
       <hr />
 
       <Routes>
