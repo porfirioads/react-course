0fd7fac2ec737b5d5dd1750a51cf0cdeedc748ad
Porfirio Ángel Díaz Sánchez <porfirioads@gmail.com>
2024-11-10 18:53:10 -0600

    feat: test default value in useCounter

---
+++ 05-hook-app/test/hooks/useCounter.test.js
@@ -0,0 +1,13 @@
+import { renderHook } from '@testing-library/react';
+import { useCounter } from '../../src/hooks/useCounter';
+
+describe('Pruebas en el useCounter', () => {
+  test('Debe retornar los valores por defecto', () => {
+    const { result } = renderHook(() => useCounter());
+    const { counter, decrement, increment, reset } = result.current;
+    expect(counter).toBe(10);
+    expect(decrement).toEqual(expect.any(Function));
+    expect(increment).toEqual(expect.any(Function));
+    expect(reset).toEqual(expect.any(Function));
+  });
+});
