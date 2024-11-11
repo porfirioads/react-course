3b2f44ff9e0335e1517d5ada50946a6e0a6d8315
Porfirio Ángel Díaz Sánchez <porfirioads@gmail.com>
2024-11-10 19:03:26 -0600

    feat: add tests for useCounter increment and decrement

--- 05-hook-app/src/hooks/useCounter.js
+++ 05-hook-app/src/hooks/useCounter.js
@@ -1,14 +1,14 @@
-import { useState } from "react";
+import { useState } from 'react';
 
 export const useCounter = (initialValue = 10) => {
   const [counter, setCounter] = useState(initialValue);
 
   const increment = (value = 1) => {
-    setCounter(counter + value);
+    setCounter((current) => current + value);
   };
 
   const decrement = (value = 1) => {
-    setCounter(counter - value > 0 ? counter - value : 0);
+    setCounter((current) => (counter - value > 0 ? counter - value : 0));
   };
 
   const reset = () => {
--- 05-hook-app/test/hooks/useCounter.test.js
+++ 05-hook-app/test/hooks/useCounter.test.js
@@ -1,5 +1,6 @@
 import { renderHook } from '@testing-library/react';
 import { useCounter } from '../../src/hooks/useCounter';
+import { act } from 'react';
 
 describe('Pruebas en el useCounter', () => {
   test('Debe retornar los valores por defecto', () => {
@@ -15,5 +16,41 @@ describe('Pruebas en el useCounter', () => {
     const { result } = renderHook(() => useCounter(100));
     const { counter } = result.current;
     expect(counter).toBe(100);
+  });
+
+  test('Debe incrementar el contador', () => {
+    const { result } = renderHook(() => useCounter(100));
+    const { increment } = result.current;
+
+    act(() => {
+      increment();
+    });
+
+    // Tomamos el counter desde result porque es un valor primitivo.
+    expect(result.current.counter).toBe(101);
+  });
+
+  test('Debe decrementar el contador', () => {
+    const { result } = renderHook(() => useCounter(100));
+    const { decrement } = result.current;
+
+    act(() => {
+      decrement();
+    });
+
+    expect(result.current.counter).toBe(99);
+  });
+
+  test('Debe realizar el reset del el contador', () => {
+    const { result } = renderHook(() => useCounter(100));
+    const { decrement, increment, reset } = result.current;
+
+    act(() => {
+      decrement();
+      increment(100)
+      reset()
+    });
+
+    expect(result.current.counter).toBe(100);
   });
 });
