947eaa73cdcd8daf3f670a9d9d4c6915d8f51bc9
Porfirio Ángel Díaz Sánchez <porfirioads@gmail.com>
2024-12-07 13:35:54 -0600

    feat: create first pages

---
+++ 06-heroes-spa/src/router/AppRouter.jsx
@@ -0,0 +1,17 @@
+import { Routes, Route, Navigate } from "react-router-dom";
+import { MarvelPage } from "../heroes/pages/MarvelPage";
+import { DcPage } from "../heroes/pages/DcPage";
+import { LoginPage } from "../auth/pages/LoginPage";
+
+export const AppRouter = () => {
+  return (
+    <>
+      <Routes>
+        <Route path="marvel" element={<MarvelPage />}></Route>
+        <Route path="dc" element={<DcPage />}></Route>
+        <Route path="login" element={<LoginPage />}></Route>
+        <Route path="/" element={<Navigate to="/marvel" />}></Route>
+      </Routes>
+    </>
+  );
+};
