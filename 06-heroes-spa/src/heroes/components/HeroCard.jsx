e9df9a6de55d1a6d9850600a340d022572424bee
Porfirio Ángel Díaz Sánchez <porfirioads@gmail.com>
2024-12-08 14:08:04 -0600

    feat: add heroes cards

---
+++ 06-heroes-spa/src/heroes/components/HeroCard.jsx
@@ -0,0 +1,38 @@
+import { Link } from "react-router-dom";
+
+export const HeroCard = ({
+  id,
+  superhero,
+  publisher,
+  alter_ego,
+  first_appearance,
+  characters,
+}) => {
+  const heroImageUrl = `/assets/heroes/${id}.jpg`;
+
+  return (
+    <div className="col">
+      <div className="card">
+        <div className="row no-gutters">
+          <div className="col-4">
+            <img src={heroImageUrl} className="card-img" alt={superhero} />
+          </div>
+
+          <div className="col-8">
+            <div className="card-body">
+              <h5 className="card-title">{superhero}</h5>
+              
+              {alter_ego !== characters && <p>{characters}</p>}
+
+              <p className="card-text">
+                <small>{first_appearance}</small>
+              </p>
+
+              <Link to={`/hero/${id}`}>Más...</Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
