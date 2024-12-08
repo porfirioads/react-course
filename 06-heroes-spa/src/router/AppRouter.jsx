01168a2fd1066eb66150a4b4559d0b9a92f23d1c
Porfirio Ángel Díaz Sánchez <porfirioads@gmail.com>
2024-12-08 10:34:57 -0600

    feat: add index.js files

--- 06-heroes-spa/src/router/AppRouter.jsx
+++ 06-heroes-spa/src/router/AppRouter.jsx
@@ -1,12 +1,12 @@
 import { Routes, Route, Navigate } from 'react-router-dom';
-import { MarvelPage } from '../heroes/pages/MarvelPage';
-import { DcPage } from '../heroes/pages/DcPage';
-import { LoginPage } from '../auth/pages/LoginPage';
+import { MarvelPage, DcPage } from '../heroes';
+import { LoginPage } from '../auth';
 import { Navbar } from '../ui';
 
 export const AppRouter = () => {
   return (
     <>
+      <Navbar />
       <Routes>
         <Route path="marvel" element={<MarvelPage />}></Route>
         <Route path="dc" element={<DcPage />}></Route>
--- 06-heroes-spa/src/ui/components/Navbar.jsx
+++ 06-heroes-spa/src/ui/components/Navbar.jsx
@@ -2,28 +2,42 @@ import { Link, NavLink } from 'react-router-dom';
 
 export const Navbar = () => {
   return (
-    <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
+    <nav className="navbar navbar-expand-sm navbar-dark bg-dark p-2">
       <Link className="navbar-brand" to="/">
         Asociaciones
       </Link>
 
       <div className="navbar-collapse">
         <div className="navbar-nav">
-          <NavLink className="nav-item nav-link" to="/marvel">
+          <NavLink
+            className={({ isActive }) =>
+              `nav-item nav-link ${isActive ? 'active' : ''}`
+            }
+            to="/marvel"
+          >
             Marvel
           </NavLink>
 
-          <NavLink className="nav-item nav-link" to="/dc">
+          <NavLink
+            className={({ isActive }) =>
+              `nav-item nav-link ${isActive ? 'active' : ''}`
+            }
+            to="/dc"
+          >
             DC
           </NavLink>
         </div>
       </div>
 
-      <div className="navbar-collapse collapse w-100 order-3 dual-collapse2">
+      <div className="navbar-collapse collapse w-100 order-3 dual-collapse2 d-flex justify-content-end">
         <ul className="navbar-nav ml-auto">
-          <NavLink className="nav-item nav-link" to="/login">
+          <span className='nav-item nav-link text-primary'>
+            Porfirio
+          </span>
+
+          <button className='nav-item nav-link btn'>
             Logout
-          </NavLink>
+          </button>
         </ul>
       </div>
     </nav>
