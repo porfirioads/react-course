0541e82ba171362b55298c0bb9a4d11f25e451a9
Porfirio Ángel Díaz Sánchez <porfirioads@gmail.com>
2024-12-14 13:58:05 -0600

    feat: add useMemo for hero page and hero list

--- 06-heroes-spa/src/heroes/components/HeroList.jsx
+++ 06-heroes-spa/src/heroes/components/HeroList.jsx
@@ -1,9 +1,10 @@
+import { useMemo } from 'react';
 import { getHeroesByPublisher } from '../helpers';
 import { HeroCard } from './HeroCard';
 import PropTypes from 'prop-types';
 
 export const HeroList = ({ publisher }) => {
-  const heroes = getHeroesByPublisher(publisher);
+  const heroes = useMemo(() => getHeroesByPublisher(publisher), [publisher]);
 
   return (
     <div className="row row-cols-1 row-cols-md-3 g-3">
