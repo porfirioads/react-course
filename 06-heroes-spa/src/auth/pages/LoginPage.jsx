737f27a803ae54cea9532a35f0be17d1d1f6fc18
Porfirio Ángel Díaz Sánchez <porfirioads@gmail.com>
2024-12-15 14:03:18 -0600

    feat: add authReducer to handle authentication

--- 06-heroes-spa/src/auth/pages/LoginPage.jsx
+++ 06-heroes-spa/src/auth/pages/LoginPage.jsx
@@ -1,10 +1,14 @@
+import { useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { AuthContext } from '../context/AuthContext';
 
 export const LoginPage = () => {
+  const { login } = useContext(AuthContext);
   const navigate = useNavigate();
 
   const onLogin = () => {
-    navigate('/marvel', { replace: true });
+    login('Porfirio Díaz');
+    navigate('/', { replace: true });
   };
 
   return (
