9a854c0311a2ed543dceaec5a18f0c3d0a941b51
Porfirio Ángel Díaz Sánchez <porfirioads@gmail.com>
2024-12-15 14:30:00 -0600

    feat: add public and private routes

--- 06-heroes-spa/src/router/AppRouter.jsx
+++ 06-heroes-spa/src/router/AppRouter.jsx
@@ -1,13 +1,30 @@
 import { Routes, Route } from 'react-router-dom';
 import { LoginPage } from '../auth';
 import { HeroesRoutes } from '../heroes';
+import { PrivateRoute } from './PrivateRoute';
+import { PublicRoute } from './PublicRoute';
 
 export const AppRouter = () => {
   return (
     <>
       <Routes>
-        <Route path="login" element={<LoginPage />}></Route>
-        <Route path="/*" element={<HeroesRoutes />}></Route>
+        <Route
+          path="/login"
+          element={
+            <PublicRoute>
+              <LoginPage />
+            </PublicRoute>
+          }
+        />
+
+        <Route
+          path="/*"
+          element={
+            <PrivateRoute>
+              <HeroesRoutes />
+            </PrivateRoute>
+          }
+        />
       </Routes>
     </>
   );
