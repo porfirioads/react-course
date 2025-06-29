cc760d2e53e1210560eff1819544567b13fff18c
Porfirio Ángel Díaz Sánchez <porfirioads@gmail.com>
2025-06-29 17:02:18 -0600

    feat: add journal page and layout

--- 07-journal-app/src/journal/pages/JournalPage.jsx
+++ 07-journal-app/src/journal/pages/JournalPage.jsx
@@ -1,11 +1,21 @@
 import { Typography } from '@mui/material';
 import MailOutline from '@mui/icons-material/MailOutline';
+import { JournalLayout } from '../layout';
+
+const drawerWidth = 240;
 
 export const JournalPage = () => {
   return (
-    <>
-      <Typography variant={'h1'}>JournalPage</Typography>
-      <MailOutline />
-    </>
+    <JournalLayout>
+      <Typography>
+        Officia elit Lorem ad anim pariatur esse anim fugiat ullamco deserunt.
+        Adipisicing aliquip deserunt aliqua esse Lorem ad veniam sit anim nulla.
+        Consequat commodo esse officia id sint. Ea culpa laboris labore sint
+        consectetur minim cillum minim laborum amet ipsum. Ex aliquip duis
+        cupidatat voluptate laboris eu aliquip consequat ullamco excepteur qui
+        pariatur. Laborum nisi velit quis exercitation magna adipisicing Lorem
+        cupidatat officia quis ullamco est irure.
+      </Typography>
+    </JournalLayout>
   );
 };
