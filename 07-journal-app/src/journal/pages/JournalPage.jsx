b3e4ae4f12829c7fd62427972190f2ced6a8d1fd
Porfirio Ángel Díaz Sánchez <porfirioads@gmail.com>
2025-06-30 00:05:08 -0600

    feat: add nothing selected view

--- 07-journal-app/src/journal/pages/JournalPage.jsx
+++ 07-journal-app/src/journal/pages/JournalPage.jsx
@@ -1,19 +1,10 @@
-import { Typography } from '@mui/material';
-import MailOutline from '@mui/icons-material/MailOutline';
 import { JournalLayout } from '../layout';
+import { NothingSelectedView } from '../views';
 
 export const JournalPage = () => {
   return (
     <JournalLayout>
-      <Typography>
-        Officia elit Lorem ad anim pariatur esse anim fugiat ullamco deserunt.
-        Adipisicing aliquip deserunt aliqua esse Lorem ad veniam sit anim nulla.
-        Consequat commodo esse officia id sint. Ea culpa laboris labore sint
-        consectetur minim cillum minim laborum amet ipsum. Ex aliquip duis
-        cupidatat voluptate laboris eu aliquip consequat ullamco excepteur qui
-        pariatur. Laborum nisi velit quis exercitation magna adipisicing Lorem
-        cupidatat officia quis ullamco est irure.
-      </Typography>
+      <NothingSelectedView />
     </JournalLayout>
   );
 };
